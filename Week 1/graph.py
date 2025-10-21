84a52d8c3598989d8b78a379ae53e2aedb686207
Parn <keskarn.parn@gmail.com>
2025-10-21 12:03:22 +0700

    Create graph.py

---
+++ Week 1/graph.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+data = pd.read_csv(r"Week 1\train.csv")
+print(data.shape)
+print(data.head())
+
+y_train = data['label']
+x_train = data.drop('label',axis=1)
+
+image = x_train.iloc[8].values
+image = image.reshape(28,28)
+
+plt.imshow(image, cmap='gray')
+plt.title(f'Label: {y_train.iloc[8]}')
+plt.show()
