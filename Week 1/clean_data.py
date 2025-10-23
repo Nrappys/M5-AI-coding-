d3526dd77ab1feabe07718c40370f40ae5003609
Parn <keskarn.parn@gmail.com>
2025-10-23 19:03:28 +0700

    Update clean_data.py

--- Week 1/clean_data.py
+++ Week 1/clean_data.py
@@ -1,15 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 data = pd.read_csv(r"Week 1\train.csv")
-# print(data.shape)
-# print(data.head())
 
-# y_train = data['label']
-# x_train = data.drop('label',axis=1)
+data_re =  data.replace('',np.nan)
+missing_values = data_re.isnull().sum()
+print(missing_values)
 
-# image = x_train.iloc[8].values
-# image = image.reshape(28,28)
+print('-' * 200)
 
-for x in data :
-    print(x)
+morethan_255 = data_re > 255
+print(morethan_255.sum())
+
+y_train = data_re["label"]
+x_train = data.drop('label', axis=1)
+
+for x in range (10):
+    if x in y_train.values:
+        plt.subplot(5, 2, x + 1)
+        plt.imshow(x_train[y_train == x].values[0].reshape(28, 28), cmap='gray')
+plt.show()
